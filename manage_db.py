8e2da41676ee3a23d9187fdfc0e3b8b9b47b32ea
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-10-29 11:22:55 +0100

    m

--- manage_db.py
+++ manage_db.py
@@ -13,9 +13,9 @@ acceptedTypes = [ "Cafe",  "African",  "American",  "Argentinean",  "Asian",  "B
  
 otherTypes = {'Hamburguesas':"Hamburgers", 'Estadounidense':"Hamburgers", 'Italiana':'Italian', 'Pizza ':'Pizza', 'Pasta ':'Italian', 'Gourmet':'Gourmet', 'Alcohol':'Bar', 'Postres':'Dessert', 'Saludable':'Healthy', 'Hawaiana':'Hawaiian', 'Pollo ':'American', 'Ensaladas ':'Healthy', 'Sushi':'Sushi', 'Japonesa':'Sushi', 'Asiática':'Asian', 'Cocina vegana':'Vegan Options','Cocina vegetariana':'Vegetarian Friendly', 'Mediterránea':'Mediterranean', 'Alitas de pollo ':'American','Turca':'Turkish', 'Tailandesa':'Thai', 'Comida callejera':'Street Food', 'Cafe':'Cafe', 'Cafe y té':'Cafe', 'Zumos y batidos':'Cafe','China':'Chinese', 'Fusión asiática':'Fusion', 'Noodles':'Asian', 'India':'Indian', 'Barbacoa japonesa':'Barbeque', 'Brochetas de pollo al estilo japonés':'Barbeque', 'Aus Burger':'Hamburgers', 'Mexicana':'Mexican', 'Tex Mex':'Mexican', 'De Nuevo México':'Mexican', 'Estadounidense tradicional':'American', 'Vegana':'Vegan Options', 'Vegetariana':'Vegetarian Friendly', 'Latinoamericana':'South American', 'Fusión latina':'South American', 'Argentina':'Argentinean', 'Española':'Mediterranean', 'Boles de arroz':'Asian', 'Tapas y raciones':'Mediterranean', 'Venezolana':'South American', 'Mariscos':'Seafood', 'Helado y yogur helado':'Dessert', 'Desayuno y brunch':'Dessert', 'Cafetería':'Cafe', 'Pasteles':'Dessert', 'Brasileña':'Grill', 'Helado y yogur helado':'Dessert', 'Yogur helado':'Dessert', 'Carnicería':'Grill', 'Sudamericana':'South American', 'Panadería':'Dessert', 'Barbacoa':'Barbeque', 'Francesa':'French', 'Curry indio':'indian', 'Dulces japoneses':'Dessert', 'Burgers':'Hamburgers', 'Drinks':'Bar', 'Vegetariano puro':'Vegetarian Friendly', 'Fish & chips':'Seafood', 'Cafe':'Cafe', 'Griega':'Greek', 'Ice Cream & Frozen Yogurt':'Dessert', 'Desserts':'Dessert', 'Wine':'Wine Bar', 'Licorerías':'Bar', "Coreana":"Korean",'Pollo':'American', 'Americana':'American', 'Tailandesa':'Thai', 'Medio Oriental':'Asian', 'Americana Hamburguesas':'Hamburgers', 'Italiana':'Italian', 'Pizza':'Pizza', 'Pollo Americana':'American', 'Venezolana':'South American', 'Sudamericana':'South American', 'Mediterránea':'Mediterranean', 'Japonesa':'Sushi', 'Sushi':'Sushi', 'Hamburguesas':'Hamburgers', 'Tapas':'Mediterranean', 'China':'Chinese', 'Oriental':'Asian', 'India':'Indian', 'Kebab':'Turkish', 'Mexicana':'Mexican', 'Tex-Mex':'Mexican', 'Española':'Mediterranean', 'Gourmet':'Gourmet', 'Brasería':'Grill', 'Colombiana':'South American', 'Repostería':'Dessert', 'Fusión':'Fusion', 'Arroceria':'Mediterranean', 'Paellas':'Mediterranean', 'Heladería':'Dessert', 'Repostería':'Dessert', 'Brasileña':'Grill', 'Turca':'Turkish', 'Vegetariana':'Vegetarian Friendly', 'Sana':'Healthy', 'Vegana':'Vegan Options', 'Desayuno':'Dessert', 'Poke':'Hawaiian',"Coreana":"Korean"}
 
-def uploadJson(filename):
+def uploadJson(filename,numb):
     data = json.load(open(filename, encoding='utf-8'))
-    restaurant = data[str(331)]
+    restaurant = data[str(numb)]
     types = []
     if "id" not in restaurant:
         for cuisine in restaurant["cuisine"]:
@@ -106,7 +106,6 @@ def uploadJson(filename):
     
     response = requests.request("POST",ip + "/restaurants", data = body)
     restaurant_id = json.loads(response.text)[0]["restaurant_id"]
-    print(restaurant_id)
 
     sections = []
     finalMenu = []
@@ -131,7 +130,7 @@ def uploadJson(filename):
                 "restaurant_id": str(restaurant_id), 
                 "name" : element["name"],
                 "section" : section,
-                "price" : price,
+                "price" : price.replace(",", "."),
                 "image" : element["image"],
                 "pos" : str(i),
                 "description" : element["description"],
@@ -146,11 +145,11 @@ def uploadJson(filename):
     })
     print(response.text)
     for entry in finalMenu:
-        response = requests.request("POST", ip + "/menus", data = str(entry).replace("'", "\"").encode('utf-8')) #no se envia bien
+        response = requests.request("POST", ip + "/menus", data = entry) #no se envia bien
         print(response.text)
         
 
 
 def main(): 
-  uploadJson(sys.argv[1])
+  uploadJson(sys.argv[1], sys.argv[2])
 main()
